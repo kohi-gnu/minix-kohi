889a96a234b92718371142f122871c0aaebd5eea
d0p1 <contact@d0p1.eu>
2022-05-21 09:04:07 +0200

    remove netbsd source tree

--- sys/arch/evbarm/include/cpu_counter.h
+++
@@ -1,3 +0,0 @@
-/* $NetBSD: cpu_counter.h,v 1.1 2012/08/14 21:12:59 matt Exp $ */
-
-#include <arm/cpu_counter.h>
--- sys/arch/evbarm/include/netbsd32_machdep.h
+++
@@ -1,3 +0,0 @@
-/* $NetBSD: netbsd32_machdep.h,v 1.1 2012/08/03 08:00:17 matt Exp $ */
-
-#include <arm/netbsd32_machdep.h>
